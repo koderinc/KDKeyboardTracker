b4380771ee1c937c1d3db34e7d8dc6f207534e93
Aaron Satterfield <aasatt@gmail.com>
2020-02-14 11:07:02 -0800

    Use preconditionFailure instead of fatalError

--- KDKeyboardTracker-Example/KDKeyboardTracker-Example/ViewController.swift
+++ KDKeyboardTracker-Example/KDKeyboardTracker-Example/ViewController.swift
@@ -29,7 +29,7 @@ class CustomView: UIView {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        preconditionFailure("init(coder:) has not been implemented")
     }
     
 }
--- README.md
+++ README.md
@@ -87,7 +87,7 @@ class CustomView: UIView {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        preconditionFailure("init(coder:) has not been implemented")
     }
     
 }
